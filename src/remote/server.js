62f772701d05e017246ce0ca516a4f1583ee92bb
francky <francky877832@gmail.com>
2025-03-25 10:06:19 +0300

    Changes to be committed: 	modified:   src/remote/server.js

--- src/remote/server.js
+++ src/remote/server.js
@@ -2,7 +2,7 @@
 
 
 
-//export const server = 'https://dkbbackend.onrender.com'
-export const server = 'http://192.168.56.1:3000' // 192.168.56.1
+export const server = 'https://dkbbackend.onrender.com'
+//export const server = 'http://192.168.56.1:3000' // 192.168.56.1
 
 
